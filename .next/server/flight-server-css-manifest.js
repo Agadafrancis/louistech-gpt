d2586322f87bbd4e004a6296bac94c32447ce59c
Francis Agada <agadafrancis91@gmail.com>
2023-05-12 16:35:40 +0100

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\New folder\\reall\\louistech-gpt\\app\\layout.tsx": [
+      "C:\\New folder\\reall\\louistech-gpt\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\New folder\\reall\\louistech-gpt\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\New folder\\reall\\louistech-gpt\\app\\page": [
+      "C:\\New folder\\reall\\louistech-gpt\\app\\globals.css",
+      "C:\\New folder\\reall\\louistech-gpt\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}

b5d8cc0a409a582700c3255a62da9b3077cfb58b
Agada Francis Louis N <57049528+Agadafrancis@users.noreply.github.com>
2023-05-15 17:19:54 +0100

    15/05/23
    building of the sidebar and body

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+import { SunIcon } from '@heroicons/react/24/outline';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Louiss</h1>
+    <div className='flex flex-col items-center justify-center h-screen px-2 text-white'>
+      <h1 className='text-5xl font-bold mb-20'>Louis GPT</h1>
+
+      <div>
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <SunIcon className="h-8 w-8 " />
+            <h2>Examples</h2>
+          </div>
+
+          <div className="space-y-2">
+            <p className="infoText">"Explain Something to me"</p>
+            <p className="infoText">"What is the diference between a cow and a beef"</p>
+            <p className="infoText">"What is the color of the moon"</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

2ee903e290518cd0ce720722d2181424e25b6bc3
Agada Francis Louis N <57049528+Agadafrancis@users.noreply.github.com>
2023-06-09 15:52:40 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         domains: ['scontent.fabv3-2.fna.fbcdn.net', 'links.papareact.com'],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    
 }
 
 module.exports = nextConfig
